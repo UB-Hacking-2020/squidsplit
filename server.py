5db461972a65e4410aa77aa93849814c66f7e91d
Krivokrysenko <ckrivokrysenko@gmail.com>
2020-10-24 22:45:13 -0400

    slightly more bottle stuff, python stuff finished?

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from bottle import route, run, static_file
+import RK
 
 @route('/')
 def index():
@@ -7,5 +8,11 @@ def index():
 @route('/stylesheet.css')
 def style():
     return static_file("stylesheet.css", root='')
+
+@route('/splicer')
+def splice(youtubeURL):
+    RK.downloadAsMP3(youtubeURL)
+    IBMjson = RK.youtubeIBManalysis(youtubeURL[-11:])
+    RK.mp3AndTimestampsToSpliced(IBMjson, youtubeURL[-11:])
 
 run(host='localhost', port=8080, debug=True)
